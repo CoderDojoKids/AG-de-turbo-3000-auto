ded3472c59a41551b02e8b90d6edb89d91e26453
CoderDojoKids <128733393+CoderDojoKids@users.noreply.github.com>
2023-06-24 11:44:53 +0200

    beter rijden

--- main.ts
+++ main.ts
@@ -7,18 +7,11 @@ radio.onReceivedNumber(function (receivedNumber) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     } else if (receivedNumber == 6) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    } else if (receivedNumber == 5) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
     } else {
     	
     }
-})
-input.onButtonPressed(Button.A, function () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 111)
-})
-input.onButtonPressed(Button.AB, function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 111)
-})
-input.onButtonPressed(Button.B, function () {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 111)
 })
 basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
